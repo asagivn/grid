d94cc17827c96f61a1b71c5c5284080d59fe5913
NQT <thang@nqt.local>
2015-11-08 09:51:55 +0900

    Initial Commit

---
+++ GridView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GridView
+//
+//  Created by NQT on 11/8/15.
+//  Copyright © 2015 NQT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
